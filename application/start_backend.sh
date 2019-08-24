4abd320050ca7b8cc3efcc67ce0804583bf848da
Shezad Aibara <shezad@artoo.in>
2019-08-24 18:49:45 +0530

    started django project

---
+++ application/start_backend.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Start Gunicorn processes
+echo Running Migration.
+# python manage.py migrate
+
+echo Starting Gunicorn.
+gunicorn application.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 2
